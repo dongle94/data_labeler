6aa81e79357377e028d63add432bd06986157849
dongle94 <sdy1029@gmail.com>
2024-04-16 19:04:19 +0900

    Feat: pyside main.py
    - 출력한 mainwindow ui 적용

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Slot
 from ui.ui_mainwindow import Ui_MainWindow
 
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     # ...
-    label = QLabel('HELLO PYSIDE')
-    label.show()
+
+    window = MainWindow()
+    window.show()
+
     sys.exit(app.exec())
