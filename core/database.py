81caa0087ca8185a0375dabf2209508d2f33cf94
dongle94 <sdy1029@gmail.com>
2024-04-29 13:30:07 +0900

    Feat: dataset
    - 데이터베이스 매니저 로딩 시 로거 추가
    - 쿼리 오탈자 수정
    - 데이터셋 조회 구문을 이름에 의한 한줄 쿼리와 전체 쿼리로 구분
      - 데이터셋 별 탭을 그리기 위해서는 전체 쿼리 필요

--- core/database.py
+++ core/database.py
@@ -21,6 +21,8 @@ class DBManager(object):
         )
 
         self.logger = logger
+        if self.logger is not None:
+            self.logger.info("Initializing database")
 
     def show_tables(self):
         with self.con.cursor() as cursor:
@@ -30,7 +32,7 @@ class DBManager(object):
             print(r)
 
     def create_dataset(self, name, data_type, dsec=""):
-        sql = "INSERT INTO dataset (name, data_type, data_dsec) VALUES (%s %s %s)"
+        sql = "INSERT INTO dataset (name, data_type, data_desc) VALUES (%s, %s, %s)"
         data = (name, data_type, dsec)
 
         cursor = self.con.cursor()
@@ -38,11 +40,21 @@ class DBManager(object):
         self.con.commit()
         cursor.close()
 
-        self.logger.info("Dataset %s created", name)
+        self.logger.info("Dataset '%s' created", name)
 
-    def read_dataset(self, name):
+    def read_dataset(self):
+        sql = "SELECT * FROM dataset"
+
+        cursor = self.con.cursor()
+        cursor.execute(sql)
+        ret = cursor.fetchall()
+        cursor.close()
+
+        return ret
+
+    def read_dataset_detail(self, name):
         sql = "SELECT * FROM dataset WHERE name = (%s)"
-        data = (name, )
+        data = tuple(name)
 
         cursor = self.con.cursor()
         cursor.execute(sql, data)
