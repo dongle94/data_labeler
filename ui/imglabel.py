83e6231158825b2e6a78112bd166f4604f43efd7
dongle94 <sdy1029@gmail.com>
2024-04-29 23:57:14 +0900

    Feat: ImgWidget
    - 이미지 라벨에 기본 백그라운드 라벨은 이미지, 박스 오버레이 라벨을 담고 있는 이미지 위젯 추가

--- ui/imglabel.py
+++ ui/imglabel.py
@@ -1,8 +1,8 @@
 import numpy
 
-from PySide6.QtWidgets import QLabel
+from PySide6.QtWidgets import QLabel, QWidget, QVBoxLayout
 from PySide6.QtCore import Signal, QRect
-from PySide6.QtGui import QPainter, QPaintEvent, QPolygon, QPen, QColor, QBrush, Qt
+from PySide6.QtGui import QPainter, QPaintEvent, QPolygon, QPen, QColor, QBrush, Qt, QPixmap, QImage
 
 
 class ImgLabel(QLabel):
@@ -37,3 +37,35 @@ class BoxOverlayLabel(ImgLabel):
         qp.drawRect(rect)
 
         qp.end()
+
+
+class ImgWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self.bg_label = ImgLabel()
+
+        layout = QVBoxLayout()
+        self.img_labels = []
+        layout.addWidget(self.bg_label)
+
+        self.setLayout(layout)
+
+    def set_image(self, img, scale=False):
+        self.bg_label.setPixmap(QPixmap().fromImage(img))
+        self.bg_label.setScaledContents(scale)
+
+    def set_array(self, arr, scale=False):
+        img = QImage(arr.data, arr.shape[1], arr.shape[0], QImage.Format.Format_BGR888)
+        self.set_image(img)
+        self.bg_label.setScaledContents(scale)
+
+    def set_file(self, path, scale=False):
+        qpixmap = QPixmap().load(path)
+        self.bg_label.setPixmap(qpixmap)
+        self.bg_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.bg_label.setScaledContents(scale)
+
+    def add_box(self):
+        self.img_labels.append(BoxOverlayLabel())
+        # TODO add box
