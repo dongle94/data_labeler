80d55f794653aff3250ddba22679483f0acca056
dongle94 <sdy1029@gmail.com>
2024-05-21 16:01:04 +0900

    Fixed: weed.sh
    - 옵션 순서에 따라 적용되지 않는 문제 해결
    - 디렉토리를 제일 먼저 써줘야 실제 서버의 디렉토리 경로로 활용가능

--- scripts/weed.sh
+++ scripts/weed.sh
@@ -3,10 +3,10 @@
 cd "$(dirname "$0")/.."
 
 ./core/seaweedfs/weed server \
+  -dir=/data/weed \
   -master.port=9333 \
   -master.volumeSizeLimitMB=29999 \
   -volume.port=8080 \
   -volume.max=8 \
   -filer \
   -filer.port=8888
-  -dir=/data/weed
