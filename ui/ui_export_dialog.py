ffa364dddce8dc54cde49a0cc61f32efd65d72a9
dongle94 <sdy1029@gmail.com>
2024-07-09 18:11:48 +0900

    Style: split dataset
    - export_dialog.ui, ui_export_dialog.py
    - train/val/test 데이터 분할 비율입력 gui 추가
    - shuffle 기능 체크박스 추가

--- ui/ui_export_dialog.py
+++ ui/ui_export_dialog.py
@@ -15,16 +15,17 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
-    QHBoxLayout, QLineEdit, QSizePolicy, QToolButton,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QCheckBox, QDialog,
+    QDialogButtonBox, QHBoxLayout, QLabel, QLineEdit,
+    QSizePolicy, QSpacerItem, QToolButton, QVBoxLayout,
+    QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
         Dialog.setWindowModality(Qt.ApplicationModal)
-        Dialog.resize(350, 76)
+        Dialog.resize(350, 140)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -50,6 +51,78 @@ class Ui_Dialog(object):
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy1)
+        self.label.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout_2.addWidget(self.label)
+
+        self.le_train = QLineEdit(Dialog)
+        self.le_train.setObjectName(u"le_train")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.le_train.sizePolicy().hasHeightForWidth())
+        self.le_train.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout_2.addWidget(self.le_train)
+
+        self.label_2 = QLabel(Dialog)
+        self.label_2.setObjectName(u"label_2")
+        sizePolicy1.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy1)
+        self.label_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout_2.addWidget(self.label_2)
+
+        self.le_val = QLineEdit(Dialog)
+        self.le_val.setObjectName(u"le_val")
+        sizePolicy2.setHeightForWidth(self.le_val.sizePolicy().hasHeightForWidth())
+        self.le_val.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout_2.addWidget(self.le_val)
+
+        self.label_3 = QLabel(Dialog)
+        self.label_3.setObjectName(u"label_3")
+        sizePolicy1.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy1)
+        self.label_3.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout_2.addWidget(self.label_3)
+
+        self.le_test = QLineEdit(Dialog)
+        self.le_test.setObjectName(u"le_test")
+        sizePolicy2.setHeightForWidth(self.le_test.sizePolicy().hasHeightForWidth())
+        self.le_test.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout_2.addWidget(self.le_test)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.checkBox = QCheckBox(Dialog)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setAutoExclusive(False)
+        self.checkBox.setTristate(False)
+
+        self.horizontalLayout_3.addWidget(self.checkBox)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+
         self.buttonBox = QDialogButtonBox(Dialog)
         self.buttonBox.setObjectName(u"buttonBox")
         self.buttonBox.setOrientation(Qt.Horizontal)
@@ -68,5 +141,13 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Set Export Path", None))
         self.toolButton.setText(QCoreApplication.translate("Dialog", u"Set Path", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"train:", None))
+        self.le_train.setText("")
+        self.le_train.setPlaceholderText(QCoreApplication.translate("Dialog", u"0.9", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"val:", None))
+        self.le_val.setPlaceholderText(QCoreApplication.translate("Dialog", u"0.1", None))
+        self.label_3.setText(QCoreApplication.translate("Dialog", u"test:", None))
+        self.le_test.setPlaceholderText(QCoreApplication.translate("Dialog", u"0.0", None))
+        self.checkBox.setText(QCoreApplication.translate("Dialog", u"Shuffle", None))
     # retranslateUi
 
